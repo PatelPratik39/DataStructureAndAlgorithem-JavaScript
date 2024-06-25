216269ddaa563e8fb2cd9e9c5f0c397994d420d1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-24 22:56:36 -0500

    Drop Non-Dominants O(n^2) and O(n)  = O(n^2 + n)

--- DropConstant.java
+++ DropConstant.java
@@ -3,21 +3,22 @@
 DROP Constant
 
  */
-
+/*
+ n * n = n^2
+ O(n^2)
+ */
 
 public class DropConstant {
 
     public static void printItems(int n){
         for(int i= 0; i < n; i++){
-            System.out.println(i);
-        }
-        for(int j= 0; j < n; j++){
-            System.out.println(j);
+            for(int j= 0; j < n; j++){
+                System.out.println(j + " " + j);
+            }
         }
     }
 
     public static void main ( String[] args ) {
         printItems(10);
-
     }
 }
---
+++ DropNonDominants.java
@@ -0,0 +1,36 @@
+
+/*
+
+Drop Non-Dominants
+O(n^2) and O(n)  = O(n^2 + n)
+ */
+
+
+public class DropNonDominants {
+
+//    Big O: Drop Non-constants
+
+//    public static void printItems(int n){
+//        for(int i = 0; i < n; i++){
+//            for(int j = 0; j < n; j++){
+//                System.out.println(i + " " + j);
+//            }
+//        }
+//        for(int k = 0; k < n; k++){
+//            System.out.println(k);
+//        }
+//    }
+
+//     Big O: O(1) -> Most efficient Big O
+
+    public static int addItems(int n){
+        return n + n + n;
+    }
+
+
+
+
+    public static void main ( String[] args ) {
+            printItems(10);
+    }
+}
