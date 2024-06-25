2e939d4fba4fce34af8a02d7d4ce05fe491e417a
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-24 22:57:04 -0500

    Drop Non-Dominants O(n^2) and O(n)  = O(n^2 + n)

--- DropNonDominants.java
+++ DropNonDominants.java
@@ -10,22 +10,22 @@ public class DropNonDominants {
 
 //    Big O: Drop Non-constants
 
-//    public static void printItems(int n){
-//        for(int i = 0; i < n; i++){
-//            for(int j = 0; j < n; j++){
-//                System.out.println(i + " " + j);
-//            }
-//        }
-//        for(int k = 0; k < n; k++){
-//            System.out.println(k);
-//        }
-//    }
+    public static void printItems(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.println(i + " " + j);
+            }
+        }
+        for(int k = 0; k < n; k++){
+            System.out.println(k);
+        }
+    }
 
 //     Big O: O(1) -> Most efficient Big O
 
-    public static int addItems(int n){
-        return n + n + n;
-    }
+//    public static int addItems(int n){
+//        return n + n + n;
+//    }
 
 
 
