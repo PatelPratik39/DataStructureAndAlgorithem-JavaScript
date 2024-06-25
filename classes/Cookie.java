0a13d20dd5d2c50ce38c19606a2ce88106cce67b
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-24 23:36:01 -0500

    ReadMe

---
+++ classes/Cookie.java
@@ -0,0 +1,23 @@
+package classes;
+
+/*
+
+Big O -> ArrayList
+
+ */
+public class Cookie {
+
+    private String color;
+
+    public Cookie(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+}
---
+++ classes/Main.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Main {
+    public static void main ( String[] args ) {
+
+        Cookie cookieOne = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
+
+        cookieOne.setColor("yellow");
+
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
+    }
+}
