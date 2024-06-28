19bf5fb4021c5f90d7b34be83ab39dad1a0e05ff
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-28 12:23:45 -0500

    performance check for a function without bigO

--- BigO/BigO.js
+++ BigO/BigO.js
@@ -1,12 +1,27 @@
 const name = ["mowglee"];
+const names = [
+  "Bagheera",
+  "Mowgleee",
+  "Bagheera",
+  "Baloo",
+  "Sherkhan",
+  "Simba",
+  "Kaa",
+  "Zakhira",
+  "Lallu"
+];
+
+const large = new Array(10000).fill('Balloo');
 
 function findNemo(array) {
+  let time0 = performance.now();
+
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === "mowglee") {
+    if (array[i] === "Balloo") {
       console.log("Found Name");
-    } else {
-        console.log("Not Found");
     }
   }
+  let time1 = performance.now();
+  console.log("Call to find Mowglee " + (time1 - time0) + " milliseconds");
 }
-findNemo(name);
+findNemo(large);
