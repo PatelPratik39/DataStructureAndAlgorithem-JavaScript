62b6e2283f5e40dddcbc7cd5d612e52229597913
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-28 12:43:35 -0500

    O(1) -> Constant Time that is excelent

--- BigO/BigO.js
+++ BigO/BigO.js
@@ -1,25 +1,34 @@
-const name = ["mowglee"];
-const names = [
-  "Bagheera",
-  "Mowgleee",
-  "Bagheera",
-  "Baloo",
-  "Sherkhan",
-  "Simba",
-  "Kaa",
-  "Zakhira",
-  "Lallu"
-];
+// const name = ["mowglee"];
+// const names = [
+//   "Bagheera",
+//   "Mowgleee",
+//   "Bagheera",
+//   "Baloo",
+//   "Sherkhan",
+//   "Simba",
+//   "Kaa",
+//   "Zakhira",
+//   "Lallu"
+// ];
 
-const large = new Array(10000).fill('Balloo');
+// const large = new Array(10000).fill('Balloo');
 
-function findNemo(array) {
-//   let time0 = performance.now();
+// function findNemo(array) {
+// //   let time0 = performance.now();
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === "Balloo") {
-      console.log("Found Name");
-    }
-  }
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === "Balloo") {
+//       console.log("Found Name");
+//     }
+//   }
+// }
+// findNemo(large); //O(n) --> Linear Time
+
+
+const boxes = [0,1,2,3,4,5,6,7,8,9]
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]);  //O(1) operation
+  console.log(boxes[1]);  // O(1) Operation again sp total 2 single operation so constant time
 }
-findNemo(large); //O(n) --> Linear Time
+logFirstTwoBoxes(boxes);    // -> O(1) = constant Time
