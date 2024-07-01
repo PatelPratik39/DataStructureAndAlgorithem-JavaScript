52f6f339ada99517d90ee83ef57253849dce86ff
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-30 19:05:24 -0500

    linkedlist with Insertmethod

--- DataStructure/LinkedList/main.js
+++ DataStructure/LinkedList/main.js
@@ -13,6 +13,12 @@
 //   }
 // };
 
+class Node {
+  constructor(value) {
+    (this.value = value), (this.next = null);
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -24,17 +30,57 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
     return this;
   }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const newArr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      newArr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return newArr;
+  }
+  insert(index, value) {
+    // Check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer
+    this.length++;
+    return this;
+  }
+  traverseToIndex(index) {
+    // check oarams
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-console.log(myLinkedList);
+myLinkedList.prepend(6);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(3, 66);
+console.log(myLinkedList.printList());
+
